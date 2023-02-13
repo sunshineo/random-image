3ba9301eec666ac04baae6594be98565ea2194e8
Gordon Sun <gordon.sun@pipe17.com>
2023-02-12 23:04:13 -0800

    Change server to websocket

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,26 +1,35 @@
 import express from "express";
 import text2png from 'text2png';
+import * as http from 'http';
+import * as WebSocket from 'ws';
 
 const app = express();
 const port = 3000; // default port to listen
 
-// define a route handler for the default home page
-app.get( "/", ( req, res ) => {
-    const images: string[] = []
-    for (let i: number = 0; i < 5; i++) {
-        // A 4 digits random number from 1000 to 9999
-        const rand = Math.floor(Math.random() * 9000 + 1000)
-        const buffer = text2png('' + rand, {color: 'blue'})
-        images.push(buffer.toString('base64'))
-    }
+const server = http.createServer(app);
 
-    res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.end(JSON.stringify(images));
-} );
+// initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+
+wss.on('connection', (ws: WebSocket) => {
+
+    // connection is up, let's add a simple simple event
+    ws.on('message', (message: string) => {
+        // TODO: parse message for parameters
+        const images: string[] = []
+        for (let i: number = 0; i < 5; i++) {
+            // A 4 digits random number from 1000 to 9999
+            const rand = Math.floor(Math.random() * 9000 + 1000)
+            const buffer = text2png('' + rand, {color: 'blue'})
+            images.push(buffer.toString('base64'))
+        }
+        ws.send(JSON.stringify(images));
+    });
+});
 
 // start the Express server
-app.listen( port, () => {
+server.listen( port, () => {
     // tslint:disable-next-line:no-console
     console.log( `server started at http://localhost:${ port }` );
 } );
